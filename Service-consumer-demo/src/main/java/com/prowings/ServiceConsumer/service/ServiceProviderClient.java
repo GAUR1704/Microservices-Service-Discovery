884f6abd5effc28aa884f7b33134981810f64e36
GAUR1704 <gauravparate160@gmail.com>
2024-05-23 19:00:09 +0530

    ribbon example added

--- Service-consumer-demo/src/main/java/com/prowings/ServiceConsumer/service/ServiceProviderClient.java
+++ Service-consumer-demo/src/main/java/com/prowings/ServiceConsumer/service/ServiceProviderClient.java
@@ -3,7 +3,7 @@ package com.prowings.ServiceConsumer.service;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "service-provider")
+@FeignClient(name = "Service-provider-demo")
 public interface ServiceProviderClient {
     @GetMapping("/hello")
     String getHello();
